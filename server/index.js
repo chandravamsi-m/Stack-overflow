1696858b9de0d7ecbd70b472fac639681b94e375
chandravamsi-malluru <chandravamsi.malluru@gmail.com>
2023-12-07 11:01:24 +0530

    port updated on index.js file

--- server/index.js
+++ server/index.js
@@ -15,15 +15,16 @@ app.use(cors());
 
 app.get('/',(req,res) =>{
     res.send("This is a stack overflow clone API")
+    res.setHeader("Access-Control")
 })
 
 app.use('/user', userRoutes)
 app.use('/questions', questionRoutes)
 app.use('/answer', answerRoutes)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const DATABASE_URL = process.env.CONNECTION_URL
 
-mongoose.connect( DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
-    .catch((err) => console.log(err.message))
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
